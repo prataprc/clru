e317a31dec59c7b1d6ffc638267df4d1227ce604
prataprc <prataprc@gmail.com>
2021-03-15 17:53:24 +0530

    dev commit.

--- src/evictor.rs
+++ src/evictor.rs
@@ -25,37 +25,7 @@ pub fn evictor<K, V, H>(
         }
 
         // initialize vars for this iteration.
-        let mut count = 0;
-        let mut evict = false;
-        let epoch = time::UNIX_EPOCH.elapsed().unwrap() - max_old;
-        // skip the sentinel.
-        let mut node: &mut Access<K> = match head.as_ref() {
-            Access::S { next } => unsafe { next.load(SeqCst).as_mut().unwrap() },
-            _ => unreachable!(),
-        };
-        // iterate on the access-list.
-        loop {
-            evict = evict || count > max_count;
-            node = match *node.take_next() {
-                Access::T { next, deleted, .. } if deleted.load(SeqCst) => {
-                    node.set_next(next.unwrap());
-                    node.get_next_mut()
-                }
-                Access::T {
-                    key, born, next, ..
-                } if evict || born < epoch => {
-                    map.remove(&key);
-                    node.set_next(next.unwrap());
-                    node.get_next_mut()
-                }
-                Access::T { .. } => {
-                    count += 1;
-                    node.get_next_mut()
-                }
-                Access::N => break,
-                _ => unreachable!(),
-            }
-        }
+        do_eviction(max_count, max_old, &mut map, head.as_ref())
     }
 
     let _node: Box<Access<K>> = match head.as_ref() {
@@ -63,4 +33,48 @@ pub fn evictor<K, V, H>(
         _ => unreachable!(),
     };
     // _node drop the entire chain of access list.
+}
+
+fn do_eviction<K, V, H>(
+    max_count: usize,
+    max_old: Duration,
+    map: &mut Map<K, Cache<K, V>, H>,
+    head: &Access<K>,
+) where
+    K: Clone + PartialEq + Hash,
+    V: Clone,
+    H: BuildHasher,
+{
+    let mut count = 0;
+    let mut evict = false;
+    let epoch = time::UNIX_EPOCH.elapsed().unwrap() - max_old;
+
+    // skip the sentinel.
+    let mut node: &mut Access<K> = match head {
+        Access::S { next } => unsafe { next.load(SeqCst).as_mut().unwrap() },
+        _ => unreachable!(),
+    };
+    // iterate on the access-list.
+    loop {
+        evict = evict || count > max_count;
+        node = match *node.take_next() {
+            Access::T { next, deleted, .. } if deleted.load(SeqCst) => {
+                node.set_next(next.unwrap());
+                node.get_next_mut()
+            }
+            Access::T {
+                key, born, next, ..
+            } if evict || born < epoch => {
+                map.remove(&key);
+                node.set_next(next.unwrap());
+                node.get_next_mut()
+            }
+            Access::T { .. } => {
+                count += 1;
+                node.get_next_mut()
+            }
+            Access::N => break,
+            _ => unreachable!(),
+        }
+    }
 }
